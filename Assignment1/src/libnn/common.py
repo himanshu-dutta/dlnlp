64e85c437de0c9b681785ab29374e19a5c32b957
Himanshu Dutta <meet.himanshu.dutta@gmail.com>
2024-01-22 18:32:20 +0530

    added momentum term to SGD

--- Assignment1/src/libnn/common.py
+++ Assignment1/src/libnn/common.py
@@ -4,4 +4,5 @@ import numpy as np
 class Param:
     def __init__(self, data: np.ndarray):
         self.d: np.ndarray = data
-        self.grad: np.ndarray = None
+        self.grad: np.ndarray = np.zeros(shape=self.d.shape)
+        self.v: np.ndarray = np.zeros(shape=self.d.shape)
--- Assignment1/src/libnn/optim.py
+++ Assignment1/src/libnn/optim.py
@@ -4,14 +4,17 @@ from .common import Param
 
 
 class SGD:
-    def __init__(self, params: List[Param], lr: float):
+    def __init__(self, params: List[Param], lr: float, momentum_coeff: float = 0):
         self.params = params
         self.lr = lr
+        self.momentum_coeff = momentum_coeff
 
     def step(self) -> None:
         for param in self.params:
-            param.d -= self.lr * param.grad
+            param.v = self.momentum_coeff * param.v + self.lr * param.grad
+            param.d -= param.v
 
     def zero_grad(self) -> None:
         for param in self.params:
             param.grad = np.zeros(shape=param.d.shape)
+            param.v = np.zeros(shape=param.d.shape)
