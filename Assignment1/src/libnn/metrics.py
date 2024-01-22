7ff11c11e67ea67ee20d3f46f84df786a4fd4323
Himanshu Dutta <meet.himanshu.dutta@gmail.com>
2024-01-22 18:32:57 +0530

    added precision and recall

--- Assignment1/src/libnn/metrics.py
+++ Assignment1/src/libnn/metrics.py
@@ -2,4 +2,25 @@ import numpy as np
 
 
 def accuracy(true: np.ndarray, pred: np.ndarray) -> np.ndarray:
+    true = true.reshape(pred.shape)
+    assert true.shape == pred.shape, "shape should be same"
     return (true == pred).mean()
+
+
+def precision(true: np.ndarray, pred: np.ndarray) -> np.ndarray:
+    true = true.reshape(pred.shape)
+    assert true.shape == pred.shape, "shape should be same"
+    TP = ((pred == 1) & (true == 1)).sum()
+    FP = ((pred == 1) & (true == 0)).sum()
+    print(f"TP: {TP}, FP: {FP}")
+    precision = TP / (TP + FP)
+    return precision
+
+
+def recall(true: np.ndarray, pred: np.ndarray) -> np.ndarray:
+    true = true.reshape(pred.shape)
+    assert true.shape == pred.shape, "shape should be same"
+    TP = ((pred == 1) & (true == 1)).sum()
+    FN = ((pred == 0) & (true == 1)).sum()
+    recall = TP / (TP + FN)
+    return recall
