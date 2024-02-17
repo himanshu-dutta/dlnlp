900f3021e0c586ded0e5867702b76576d3c9c973
Himanshu Dutta <meet.himanshu.dutta@gmail.com>
2024-02-17 17:47:16 +0530

    v2

--- Assignment1/src/libnn/activations.py
+++ Assignment1/src/libnn/activations.py
@@ -20,9 +20,30 @@ class ReLU:
 
     def _backward(self) -> Callable[[np.ndarray], np.ndarray]:
         def _inner(grads_in: np.ndarray) -> np.ndarray:
-            grads_out = grads_in * np.where(self._inp > 0, 1, 0)
-            # print(f"ReLU:: grads_in.shape: {grads_in.shape}")
-            # print(f"ReLU:: calling grad function: {self._grad_fn}")
+            grads_out = grads_in * (self._inp > 0).astype(float)
+            self._grad_fn(grads_out)
+
+        return _inner
+
+
+class NormalAct:
+    def __init__(self, inplace: bool = False):
+        self.inplace = inplace
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        out = self.forward(x)
+        self._grad_fn = mem.backward_fn
+        mem.backward_fn = self._backward()
+        return out
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        self._inp = x
+        self._out = np.exp((-(x**2)) / 2)
+        return self._out
+
+    def _backward(self) -> Callable[[np.ndarray], np.ndarray]:
+        def _inner(grads_in: np.ndarray) -> np.ndarray:
+            grads_out = grads_in * (-self._out * self._inp)
             self._grad_fn(grads_out)
 
         return _inner
@@ -44,26 +65,7 @@ class Sigmoid:
 
     def _backward(self) -> Callable[[np.ndarray], np.ndarray]:
         def _inner(grads_in: np.ndarray) -> np.ndarray:
-            grads_in = grads_in.reshape(self._sigma.shape)
-            grads_out = grads_in * (self._sigma * (1 - self._sigma))
-            # print(
-            #     f"Sigmoid:: grads_in.shape: {grads_in.shape}, self._sigma.shape: {self._sigma.shape}, grads_out.shape: {grads_out.shape}"
-            # )
-            # print(f"Sigmoid:: calling grad function: {self._grad_fn}")
-            self._grad_fn(grads_out)
+            # grads_out = grads_in * (self._sigma * (1 - self._sigma))
+            self._grad_fn(grads_in)
 
         return _inner
-
-
-class Softmax:
-    def __init__(self, dim: int = None):
-        self.dim: int = dim
-
-    def __call__(self, x: np.ndarray) -> np.ndarray:
-        return self.forward(x)
-
-    def forward(self):
-        pass
-
-    def _backward(self):
-        pass
