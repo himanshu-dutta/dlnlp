e6ffb4059811133e9f3c00efe55a7032196b1eb0
Himanshu Dutta <meet.himanshu.dutta@gmail.com>
2024-03-29 23:12:22 +0530

    reverted sum back to mean

--- Assignment2/src/perceptron.py
+++ Assignment2/src/perceptron.py
@@ -25,7 +25,7 @@ class RecurrentPerceptron:
 
     def _init_weights(self):
         self.W = xavier_init((self.num_inputs, 1))
-        self.V = xavier_init((1, 1))
+        self.V = np.random.randn(1, 1) * 0.01
         self.B = xavier_init((1, 1))
 
     def _init_state(self, reset_momentum: bool = False):
@@ -133,9 +133,9 @@ class RecurrentPerceptron:
             dl_db_cache.append(dl_db)
             dl_dv_cache.append(dl_dv)
 
-        dl_dw = np.stack(dl_dw_cache, axis=0).sum(axis=0)
-        dl_db = np.stack(dl_db_cache, axis=0).sum()
-        dl_dv = np.stack(dl_dv_cache, axis=0).sum()
+        dl_dw = np.stack(dl_dw_cache, axis=0).mean(axis=0)
+        dl_db = np.stack(dl_db_cache, axis=0).mean()
+        dl_dv = np.stack(dl_dv_cache, axis=0).mean()
 
         self.cache["dl_dw"] = dl_dw
         self.cache["dl_db"] = dl_db
